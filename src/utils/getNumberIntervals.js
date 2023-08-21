76cd3b4898854c045f65de831ad7be14e32c163d
ArturiaTW <ArturiaTW@gmail.com>
2023-08-21 15:35:37 +0800

    實作找出數字 0 到 20 間重疊與未包含的數字區間 function

---
+++ src/utils/getNumberIntervals.js
@@ -0,0 +1,54 @@
+function getNumberIntervals(intervals = []) {
+  const zeroToTwenty = Array.from({ length: 21 }, (num, index) => []);
+  let index = 0;
+
+  if (!Array.isArray(intervals)) {
+    throw new Error('Please pass parameter in valid structure');
+  }
+
+  for (const [start, end] of intervals) {
+    for (let ptr = start; ptr <= end; ptr++) {
+      zeroToTwenty[ptr].push(index);
+    }
+    index++;
+  }
+
+  const overlap = [];
+  const notInclude = [];
+
+  for (let i = 0; i < zeroToTwenty.length; i++) {
+    if (!zeroToTwenty[i].length) {
+      let rPtr = i;
+      while (Array.isArray(zeroToTwenty[rPtr]) && !zeroToTwenty[rPtr].length) {
+        rPtr++;
+      }
+      notInclude.push([i, rPtr - 1]);
+      i = rPtr + 1;
+    }
+  }
+
+  for (let i = 0; i < zeroToTwenty.length; i++) {
+    if (zeroToTwenty[i].length > 1) {
+      let rPtr = i;
+      while (
+        Array.isArray(zeroToTwenty[rPtr]) &&
+        zeroToTwenty[rPtr].length > 1
+      ) {
+        rPtr++;
+      }
+      overlap.push([i, rPtr - 1]);
+      i = rPtr + 1;
+    }
+  }
+
+  return {
+    overlap,
+    notInclude,
+  };
+}
+
+const result = getNumberIntervals([
+  [3, 7],
+  [4, 5],
+  [6, 6],
+]);
