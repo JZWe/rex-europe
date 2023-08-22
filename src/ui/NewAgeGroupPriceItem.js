fb82799efaea2dde77889123603e33985f541697
ArturiaTW <ArturiaTW@gmail.com>
2023-08-22 23:36:49 +0800

    NewAgeGroupPriceItem, disabled 設定
    當所有年齡範圍已經包含 0 到 20 歲時,新增價格設定需要 disabled

--- src/ui/NewAgeGroupPriceItem.js
+++ src/ui/NewAgeGroupPriceItem.js
@@ -1,12 +1,16 @@
 import { useAgeGroup } from './AgeGroupContext';
 import { PlusIcon } from '@heroicons/react/24/solid';
+import { MIN, MAX } from '../utils/getNumberIntervals';
 import './NewAgeGroupPriceItem.css';
 
-function NewAgeGroupPriceItem({ onChange }) {
-  const { addListItem } = useAgeGroup();
+function NewAgeGroupPriceItem() {
+  const { list, addListItem } = useAgeGroup();
+  const isDisabled = list.some(
+    (item) => item.data.ageGroup?.[0] === MIN && item.data.ageGroup?.[1] === MAX
+  );
 
   return (
-    <button className="new-btn" onClick={addListItem}>
+    <button className="new-btn" disabled={isDisabled} onClick={addListItem}>
       <PlusIcon />
       <span>新增價格設定</span>
     </button>
